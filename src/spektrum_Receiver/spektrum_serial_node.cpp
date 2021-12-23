4e90f00a5de73ec4887cdad65f8fe835b42c556c
TevDickerson <tevbotbhs@gmail.com>
2021-12-22 20:56:06 -0700

    New Node

---
+++ src/spektrum_Receiver/spektrum_serial_node.cpp
@@ -0,0 +1,8 @@
+//
+//  spektrum_serial_node.cpp
+//  
+//
+//  Created by Kristin Dickerson on 12/22/21.
+//
+
+#include "spektrum_serial_node.hpp"
---
+++ src/spektrum_Receiver/spektrum_serial_node.hpp
@@ -0,0 +1,13 @@
+//
+//  spektrum_serial_node.hpp
+//  
+//
+//  Created by Kristin Dickerson on 12/22/21.
+//
+
+#ifndef spektrum_serial_node_hpp
+#define spektrum_serial_node_hpp
+
+#include <stdio.h>
+
+#endif /* spektrum_serial_node_hpp */
